996eb8a394d0078d1a973797b5f6058a24942314
Adesina <damiiyi@gmail.com>
2024-07-23 19:42:21 +0100

    updates

--- src/pages/LandingPage/components/Hero.tsx
+++ src/pages/LandingPage/components/Hero.tsx
@@ -29,12 +29,12 @@ const Hero = () => (
       </p>
     </div>
 
-    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+    <div className={`flex-1 flex ${styles.flexCenter} relative`}>
       <img
         src={robot}
         alt="billing"
-        className="w-[100%] h-[100%] 
-        relative z-[5]"
+        className="mt-4 w-[100%] h-[100%] 
+        "
       />
 
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
@@ -43,7 +43,7 @@ const Hero = () => (
     </div>
 
     <div className={`ss:hidden ${styles.flexCenter}`}>
-      <GetStarted />
+      {/* <GetStarted /> */}
     </div>
   </section>
 );
